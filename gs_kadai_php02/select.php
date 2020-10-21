09d61af1738a3849c392c54a64e5287334aac087
Kazunori-Yamawaki <69379391+Kazunori-Yamawaki@users.noreply.github.com>
2020-10-21 20:10:32 +0900

    Add files via upload

---
+++ gs_kadai_php02/select.php
@@ -0,0 +1,69 @@
+<?php
+//1.  DB接続します
+try {
+  //ID MAMP ='root'
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+//２．データ取得SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_bm_table");
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if ($status == false) {
+    //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $url = $result['url'];
+      $view .= "<tr><th>".$result['id']."</th><td>".$result['name']."</td><td><a href='$url' target='_blank'>".$result['url']."</a></td><td>".$result['comment']."</td></tr>";
+  }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ブックマーク一覧</title>
+<link rel="stylesheet" href="stylesheet.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">データ登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+
+<table>
+<tr>
+<th>番号</th><th>店名</th><th>URL</th><th>コメント</th>
+</tr>
+<?= $view ?>
+</table>
+
+<!-- Main[End] -->
+
+</body>
+</html>
